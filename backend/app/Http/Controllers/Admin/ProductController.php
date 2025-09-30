030e7944a60c64920d011274862b205ebcb4b5ba
Abdullah <abdullahxoff@gmail.com>
2025-09-30 23:26:26 +0300

    Proctuts CRUD

---
+++ backend/app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ProductRequest;
+use App\Http\Requests\Admin\ProductUpdateRequest;
+use App\Models\Product;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    use ApiResponse;
+
+    // Get All Products with Category
+    public function index()
+    {
+        $products = Product::with('category')->paginate(10);
+        return $this->success($products, 'Products List', 200);
+    }
+
+    // Create Product
+    public function store(ProductRequest $request)
+    {
+        $validated = $request->validated();
+        $product = Product::create($validated);
+        return $this->success($product, 'Product Created', 200);
+    }
+
+    // Show Product
+    public function show(Product $product)
+    {
+        return $this->success($product, 'Product Details', 200);
+    }
+
+    // Update Product
+    public function update(ProductUpdateRequest $request, Product $product)
+    {
+        $validated = $request->validated();
+        $product->update($validated);
+        return $this->success($product, 'Product Updated', 200);
+    }
+
+    // Delete Product
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return $this->success(null, 'Product Deleted', 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\Admin\AvailabilityController;
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Admin Routes
 Route::middleware(['auth:sanctum' , 'isAdmin'])->group(function () {
+    
+    // Availabilities
     Route::apiResource('admin/availability', AvailabilityController::class);
+
+    // Products
+    Route::apiResource('admin/product', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
