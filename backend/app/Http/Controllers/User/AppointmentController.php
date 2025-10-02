75167b8ec0ad003e63ba96c96787c477df9abae7
Abdullah <abdullahxoff@gmail.com>
2025-10-02 18:58:49 +0300

    Add AppointmentController for managing appointments and time slots

---
+++ backend/app/Http/Controllers/User/AppointmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+use App\Models\TimeSlot;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+
+class AppointmentController extends Controller
+{
+    use ApiResponse;
+
+    // Get All Time Slots
+    public function index()
+    {
+        $timeslots = TimeSlot::all();
+        return $this->success($timeslots, 'Time Slots', 200);
+    }
+
+    // Book Appointment
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'slot_id' => 'required|exists:time_slots,id',
+            'notes' => 'nullable|string',
+        ]);
+
+        $patient = $request->user();
+        $slotId = $validated['slot_id'];
+
+        $exsistingAppointment =
+            Appointment::query()->where('patient_id', $patient->id)
+            ->whereIn('status', ['pending', 'confirmed'])
+            ->exists();
+
+        if ($exsistingAppointment) {
+            return $this->error('You already have an active appointment', [], 409);
+        }
+
+        try {
+            $appointment = DB::transaction(function () use ($patient, $slotId, $validated) {
+
+                $timeSlot = TimeSlot::query()->where('id', $slotId)->lockForUpdate()->first();
+
+                if (!$timeSlot || $timeSlot->status !== 'available') {
+                    throw new \Exception('Slot not available or not found');
+                }
+
+                $newAppointment = Appointment::create([
+                    'patient_id' => $patient->id,
+                    'slot_id' => $timeSlot->id,
+                    'notes' => $validated['notes'] ?? null,
+                    'status' => 'confirmed',
+                ]);
+
+                $timeSlot->update(['status' => 'booked']);
+
+                return $newAppointment;
+            });
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), [], 422);
+        }
+
+        return $this->success($appointment, 'Appointment Created', 201);
+    }
+
+    // Cancel Appointment
+    public function destroy(Appointment $appointment)
+    {
+        Gate::authorize('delete', $appointment); 
+
+        try {
+            DB::transaction(function () use ($appointment) {
+                $appointment->delete();
+                TimeSlot::where('id', $appointment->slot_id)->update(['status' => 'available']);
+            });
+            return $this->success(null, 'Appointment Cancelled', 200);
+        } catch (\Throwable $th) {
+            return $this->error('An error occurred while cancelling the appointment.', [], 500);
+        }
+    }
+}
