86debf2c09a8f825bd30678d45422edb7071d8d1
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-01-15 22:49:04 +0200

    fix: update axios base URL to use HTTPS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-axios.defaults.baseURL = 'http://clinic-appointment-system.test/api/' // Backend URL
+axios.defaults.baseURL = 'https://clinic-appointment-system.test/api/' // Backend URL
 axios.defaults.withCredentials = true
 
 const token = localStorage.getItem('token')
