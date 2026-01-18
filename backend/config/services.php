39a35df7e9ec8c69f5c5f5b77ef542b88cfbfcc9
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-01-18 16:47:25 +0200

    add webhook secret key

--- backend/config/services.php
+++ backend/config/services.php
@@ -36,7 +36,8 @@ return [
     ],
 
     'stripe' => [
-        'secret' => env('STRIPE_SECRET')
+        'secret' => env('STRIPE_SECRET'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET')
     ],
 
 ];
